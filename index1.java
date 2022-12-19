9a23c4b2d9b7574dae5856ec81cd00fe483538b1
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2022-12-19 11:31:12 +0530

    Create index1.java

---
+++ index1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  //learning array
+	  //new keyword is used to allocate the memory 
+	  Scanner sc = new Scanner(System.in);
+	  int a[] = new int[10];      // declartion of array as a[]
+	  for(int i=0;i<a.length;i++){
+	     a[i] = sc.nextInt();        // giving input in the array 
+	  }
+	  System.out.println(a[3]);
+	  
+	}
+	
+}
