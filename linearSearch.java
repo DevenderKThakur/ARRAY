06429a4c19db34277569e011acdf3cf5b7a3c3bb
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2022-12-20 09:47:54 +0530

    Create linearSearch.java

---
+++ linearSearch.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// to use linear search in Java to find the position of the value in the array 
+class linearSearch
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    for(int i=0;i<n;i++);{
+      int n =sc.nextInt();
+      int a[] = new int[n];
+      for(int i=0;i<n;i++){
+          a[i] = sc.nextInt(); 
+       }
+      int x = sc.nextInt();
+      for(int i =0'i<n;i++){
+          if(a[i]==x){
+              System.out.println(i+1);
+           }  
+          if(i==n){
+             System.out.println("-1");
+          }
+        }
+    }
+	}
+}
